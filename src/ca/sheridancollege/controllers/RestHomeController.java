1ae21e3d2e83ac6da961a59bf75d34fd0061b259
Gene Tenorlas <tenorlas@sheridancollege.ca>
2018-09-29 23:33:15 -0400

    Added <security:csrf disabled=true/> in the security-context.xml to allow POST, PUT, DELETE in RESTful services. Added jackson dependencies in pom.xml

--- src/ca/sheridancollege/controllers/RestHomeController.java
+++ src/ca/sheridancollege/controllers/RestHomeController.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import ca.sheridancollege.DAO.BookingDAO;
 import ca.sheridancollege.DAO.CourtDAO;
@@ -16,12 +18,24 @@ import ca.sheridancollege.DAO.CourtDAO;
 import ca.sheridancollege.beans.Booking;
 import ca.sheridancollege.beans.Court;
 
-@RestController // specify that this class is a controller
+@RestController // specify that this class is a restful controller
+@CrossOrigin(origins = { "*"}, maxAge = 6000)
 @RequestMapping("/api")
 public class RestHomeController {
 	//private DAO dao = new DAO();
 	CourtDAO courtDAO = new CourtDAO();
 	BookingDAO bookingDAO = new BookingDAO();
+	
+	public RestHomeController(){
+		CorsRegistry registry = new CorsRegistry();
+		registry.addMapping("/api")
+		.allowedOrigins("*", "http://localhost:8080")
+		.allowedMethods("POST", "GET", "PUT", "DELETE")
+		.allowedHeaders("Content-Type")
+		.exposedHeaders("header-1", "header-2")
+		.allowCredentials(false)
+		.maxAge(6000);
+	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET) // routing to home
 	public String home(Model model) {
@@ -40,10 +54,14 @@ public class RestHomeController {
 		return bookingDAO.getBookingByID(id);
 	}
 	
+	@RequestMapping(value = "/bookingList/{courtId}", method = {RequestMethod.OPTIONS, RequestMethod.POST})
+	public String postBookingListItem(@PathVariable int courtId) {
+		return "it works" + courtId;
+	}
 
 	// new Booking(id, bookingDate, bookingType, status, startDateTime,
 	// endDateTime));
-	@RequestMapping(value = "/bookingList/{customerName}/{bookingType}/{status}/{startDateTime}/{endDateTime}/{courtId}", method = RequestMethod.GET)
+	@RequestMapping(value = "/bookingList/{customerName}/{bookingType}/{status}/{startDateTime}/{endDateTime}/{courtId}", method = {RequestMethod.OPTIONS, RequestMethod.POST})
 	public String postBookingListItem(@PathVariable String customerName,
 			@PathVariable String bookingType, @PathVariable String status, @PathVariable String startDateTime,
 			@PathVariable String endDateTime, @PathVariable int courtId) {
