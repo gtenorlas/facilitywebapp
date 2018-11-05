ea8762adaa19b90cc359534163bfdb89d9c3fe97
Gene Tenorlas <tenorlas@sheridancollege.ca>
2018-11-04 19:03:17 -0500

    updateCustomer method added to RestCustomerController

--- src/ca/sheridancollege/controllers/RestCustomerController.java
+++ src/ca/sheridancollege/controllers/RestCustomerController.java
@@ -37,63 +37,62 @@ public class RestCustomerController {
 	@RequestMapping(value = "/{username}/{password}/{originate}", method = RequestMethod.GET)
 	public Object getCustomer(@PathVariable String username, @PathVariable String password,
 			@PathVariable String originate) {
-		if (originate.equals("standard")) {
+		if (originate.toLowerCase().trim().equals("standard")) {
 			return customerDAO.getCustomer(username, password);
 		} else {
-			return customerDAO.getCustomer(username);
+			Object customer = customerDAO.getCustomer(username);
+			if (customer == null) {
+				customer = customerDAO.getCustomerByEmail(username);
+			}
+			return customer;
 		}
 	}
-	
+
 	@RequestMapping(value = "/token/{email}/", method = RequestMethod.POST)
 	public Object getToken(@PathVariable String email) {
-		Customer customer=(Customer)customerDAO.getCustomerByEmail(email.trim());
-		if (customer ==null ) {
+		Customer customer = (Customer) customerDAO.getCustomerByEmail(email.trim());
+		if (customer == null) {
 			return "invalid";
-		}else {
+		} else {
 			String token = Customer.generateToken();
 			customer.setConfirmationToken(token);
 			customerDAO.updateCustomer(customer);
-			Email newEmail = new Email(email,
-                    "Token", "You've requested a password reset for your Book2ball account login. Please enter the token below in the Book2ball app, and you'll be able to create a new password. \r\n" + 
-                    		"\r\n" + 
-                    		"Token: " + token + 
-                    		"\r\n\n\n" + 
-      
-                    		"If you have not authorized this change, please contact Book2ball with the information in this e-mail.\r\n" + 
-                    		"THANK YOU!\r\n" + 
-                    		"\r\n" + 
-                    		"MAGS.WEBSITE\r\n");
-            newEmail.send();
+			Email newEmail = new Email(email, "Token",
+					"You've requested a password reset for your Book2ball account login. Please enter the token below in the Book2ball app, and you'll be able to create a new password. \r\n"
+							+ "\r\n" + "Token: " + token + "\r\n\n\n" +
+
+							"If you have not authorized this change, please contact Book2ball with the information in this e-mail.\r\n"
+							+ "THANK YOU!\r\n" + "\r\n" + "MAGS.WEBSITE\r\n");
+			newEmail.send();
 			return "success";
 		}
 	}
-	
+
 	@RequestMapping(value = "/token/validate/{email}/{token}/", method = RequestMethod.POST)
 	public Object validateToken(@PathVariable String email, @PathVariable String token) {
-		Customer customer=(Customer)customerDAO.getCustomerByEmail(email.trim());
-		if (customer ==null ) {
+		Customer customer = (Customer) customerDAO.getCustomerByEmail(email.trim());
+		if (customer == null) {
 			return "invalid";
-		}else {
-			if(customer.getConfirmationToken().equals(token.trim())) {
+		} else {
+			if (customer.getConfirmationToken().equals(token.trim())) {
 				return "success";
-			}else {
-			return "invalid";
+			} else {
+				return "invalid";
 			}
 		}
 	}
-	
+
 	@RequestMapping(value = "/reset/{email}/{newPassword}/", method = RequestMethod.POST)
 	public Object resetPassword(@PathVariable String email, @PathVariable String newPassword) {
-		Customer customer=(Customer)customerDAO.getCustomerByEmail(email.trim());
-		if (customer ==null ) {
+		Customer customer = (Customer) customerDAO.getCustomerByEmail(email.trim());
+		if (customer == null) {
 			return "invalid";
-		}else {
+		} else {
 			customer.setPassword(Customer.hashPassword(newPassword));
 			customerDAO.updateCustomer(customer);
 			return "success";
 		}
 	}
-
 
 	// new Booking(id, bookingDate, bookingType, status, startDateTime,
 	// endDateTime));
@@ -107,12 +106,12 @@ public class RestCustomerController {
 		// String encryptedPassword = new BCryptPasswordEncoder().encode(password);
 
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy-HH-mm");
-		//LocalDateTime startDateTimeLocal = LocalDateTime.parse(startDate, formatter);
+		// LocalDateTime startDateTimeLocal = LocalDateTime.parse(startDate, formatter);
 		LocalDateTime startDateTimeLocal = LocalDateTime.now();
-		//LocalDateTime endDateTimeLocal = null;
-		//if (endDate != null && !endDate.equals("null")) {
-		//	endDateTimeLocal = LocalDateTime.parse(endDate, formatter);
-		//}
+		// LocalDateTime endDateTimeLocal = null;
+		// if (endDate != null && !endDate.equals("null")) {
+		// endDateTimeLocal = LocalDateTime.parse(endDate, formatter);
+		// }
 
 		Customer customer = new Customer(username, password, firstName, lastName, email, contactNumber,
 				startDateTimeLocal, null, status, originate, null);
@@ -122,12 +121,45 @@ public class RestCustomerController {
 			System.out.println("Email saved is: " + customer.getEmail());
 			if (saved) {
 				return "success";
-			}else {
+			} else {
 				return "Customer did not save due to internal error";
 			}
 		} else {
 			return "Customer already exists.";
 		}
+	}
+
+	// new Booking(id, bookingDate, bookingType, status, startDateTime,
+	// endDateTime));
+	@RequestMapping(value = "/update/{username}/{password}/{firstName}/{lastName}/{email}/{contactNumber}/{startDate}/{endDate}/{status}/{originate}", method = {
+			RequestMethod.OPTIONS, RequestMethod.POST })
+	public Object updateCustomer(@PathVariable String username, @PathVariable String password,
+			@PathVariable String firstName, @PathVariable String lastName, @PathVariable String email,
+			@PathVariable String contactNumber, @PathVariable String startDate, @PathVariable String endDate,
+			@PathVariable String status, @PathVariable String originate) {
+
+		// String encryptedPassword = new BCryptPasswordEncoder().encode(password);
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy-HH-mm");
+		// LocalDateTime startDateTimeLocal = LocalDateTime.parse(startDate, formatter);
+		LocalDateTime startDateTimeLocal = LocalDateTime.now();
+		// LocalDateTime endDateTimeLocal = null;
+		// if (endDate != null && !endDate.equals("null")) {
+		// endDateTimeLocal = LocalDateTime.parse(endDate, formatter);
+		// }
+
+		Customer customer = new Customer(username, password, firstName, lastName, email, contactNumber,
+				startDateTimeLocal, null, status, originate, null);
+		customer.setPassword(Customer.hashPassword(password));
+
+		boolean saved = customerDAO.updateCustomer(customer);
+		System.out.println("Email saved is: " + customer.getEmail());
+		if (saved) {
+			return "success";
+		} else {
+			return "Customer did not save due to internal error";
+		}
+
 	}
 
 }
